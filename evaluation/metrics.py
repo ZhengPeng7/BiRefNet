Fix bugs in saving ske_arrays in HCE computation. Fix bugs in infer for using compile.

evaluation/metrics.py
@@ -60,7 +60,10 @@ def evaluator(gt_paths, pred_paths, metrics=['S', 'MAE', 'E', 'F', 'WF', 'HCE'],
                 ske_ary = ske_ary > 128
             else:
                 ske_ary = skeletonize(gt_ary > 128)
-                os.makedirs(os.path.join(*ske_path.split(os.sep)[:-1]), exist_ok=True)
+                ske_save_dir = os.path.join(*ske_path.split(os.sep)[:-1])
+                if ske_path[0] == os.sep:
+                    ske_save_dir = os.sep + ske_save_dir
+                os.makedirs(ske_save_dir, exist_ok=True)
                 cv2.imwrite(ske_path, ske_ary.astype(np.uint8) * 255)
             HCE.step(pred=pred_ary, gt=gt_ary, gt_ske=ske_ary)
 

inference.py
@@ -67,7 +67,13 @@ def main(args):
             if int(weights.strip('.pth').split('ep')[-1]) % 1 != 0:
                 continue
             print('\tInferencing {}...'.format(weights))
-            model.load_state_dict(torch.load(weights, map_location='cpu'))
+            # model.load_state_dict(torch.load(weights, map_location='cpu'))
+            state_dict = torch.load(weights, map_location='cpu')
+            unwanted_prefix = '_orig_mod.'
+            for k, v in list(state_dict.items()):
+                if k.startswith(unwanted_prefix):
+                    state_dict[k[len(unwanted_prefix):]] = state_dict.pop(k)
+            model.load_state_dict(state_dict)
             model = model.to(device)
             inference(
                 model, data_loader_test=data_loader_test, pred_root=args.pred_root,

models/backbones/pvt_v2.py
@@ -383,7 +383,7 @@ def _conv_filter(state_dict, patch_size=16):
     return out_dict
 
 
-@register_model
+## @register_model
 class pvt_v2_b0(PyramidVisionTransformerImpr):
     def __init__(self, **kwargs):
         super(pvt_v2_b0, self).__init__(
@@ -393,7 +393,7 @@ class pvt_v2_b0(PyramidVisionTransformerImpr):
 
 
 
-@register_model
+## @register_model
 class pvt_v2_b1(PyramidVisionTransformerImpr):
     def __init__(self, **kwargs):
         super(pvt_v2_b1, self).__init__(
@@ -401,7 +401,7 @@ class pvt_v2_b1(PyramidVisionTransformerImpr):
             qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-6), depths=[2, 2, 2, 2], sr_ratios=[8, 4, 2, 1],
             drop_rate=0.0, drop_path_rate=0.1)
 
-@register_model
+## @register_model
 class pvt_v2_b2(PyramidVisionTransformerImpr):
     def __init__(self, in_channels=3, **kwargs):
         super(pvt_v2_b2, self).__init__(
@@ -409,7 +409,7 @@ class pvt_v2_b2(PyramidVisionTransformerImpr):
             qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-6), depths=[3, 4, 6, 3], sr_ratios=[8, 4, 2, 1],
             drop_rate=0.0, drop_path_rate=0.1, in_channels=in_channels)
 
-@register_model
+## @register_model
 class pvt_v2_b3(PyramidVisionTransformerImpr):
     def __init__(self, **kwargs):
         super(pvt_v2_b3, self).__init__(
@@ -417,7 +417,7 @@ class pvt_v2_b3(PyramidVisionTransformerImpr):
             qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-6), depths=[3, 4, 18, 3], sr_ratios=[8, 4, 2, 1],
             drop_rate=0.0, drop_path_rate=0.1)
 
-@register_model
+## @register_model
 class pvt_v2_b4(PyramidVisionTransformerImpr):
     def __init__(self, **kwargs):
         super(pvt_v2_b4, self).__init__(
@@ -426,7 +426,7 @@ class pvt_v2_b4(PyramidVisionTransformerImpr):
             drop_rate=0.0, drop_path_rate=0.1)
 
 
-@register_model
+## @register_model
 class pvt_v2_b5(PyramidVisionTransformerImpr):
     def __init__(self, **kwargs):
         super(pvt_v2_b5, self).__init__(

train.py
@@ -51,7 +51,7 @@ logger_loss_idx = 1
 
 # log model and optimizer params
 # logger.info("Model details:"); logger.info(model)
-logger.info("datasets: load_all={}, compile={}.".format(config.load_all, config.compile_and_precisionHigh))
+logger.info("datasets: load_all={}, compile={}.".format(config.load_all, config.compile))
 logger.info("Other hyperparameters:"); logger.info(args)
 print('batch size:', config.batch_size)
 
@@ -113,8 +113,9 @@ def init_models_optimizers(epochs, to_be_distributed):
         model = DDP(model, device_ids=[device])
     else:
         model = model.to(device)
-    if config.compile_and_precisionHigh:
+    if config.compile:
         model = torch.compile(model, mode=['default', 'reduce-overhead', 'max-autotune'][0])
+    if config.precisionHigh:
         torch.set_float32_matmul_precision('high')
 
 
@@ -160,7 +161,7 @@ class Trainer:
             disc = DDP(disc, device_ids=[device], broadcast_buffers=False)
         else:
             disc = disc.to(device)
-        if config.compile_and_precisionHigh:
+        if config.compile:
             disc = torch.compile(disc, mode=['default', 'reduce-overhead', 'max-autotune'][0])
         adv_criterion = nn.BCELoss()
         if config.optimizer == 'AdamW':