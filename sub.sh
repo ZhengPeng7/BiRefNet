73757f7189e0668644f1ee36c631afc31a6256da
ZhengPeng7 <zhengpeng0108@gmail.com>
2023-12-31 13:52:42 +0800

    Adapt configs to A100-40G.

--- sub.sh
+++ sub.sh
@@ -1,8 +1,8 @@
 
 # Example: ./sub.sh tmp_proj 0,1,2,3 3 --> Use 0,1,2,3 for training, release GPUs, use GPU:3 for inference.
 
-method=$1
-devices=$2
+method=${1:-"tmp"}
+devices=${2:-0}
 
 # srun --nodes=1 --nodelist=Master,Slave1,Slave2,Slave3,Slave4,Slave5 \
 # --ntasks-per-node=1 \
