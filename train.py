Fix the bug in saving state_dict in half precision.

train.py
@@ -243,7 +243,7 @@ def main():
         if epoch >= args.epochs - config.save_last and epoch % config.save_step == 0:
             if args.use_accelerate:
                 if mixed_precision == 'fp16':
-                    state_dict = trainer.model.half().state_dict()
+                    state_dict = {k: v.half() for k, v in trainer.model.state_dict().items()}
             else:
                 state_dict = trainer.model.module.state_dict() if to_be_distributed else trainer.model.state_dict()
             torch.save(state_dict, os.path.join(args.ckpt_dir, 'epoch_{}.pth'.format(epoch)))