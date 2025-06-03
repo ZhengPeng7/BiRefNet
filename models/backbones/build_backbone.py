5085740f57cc2d6e495d81533cab6adc9342bf27
Peng Zheng <zhengpeng0108@gmail.com>
2025-06-03 17:50:47 +0800

    Fix a typo in print.

--- models/backbones/build_backbone.py
+++ models/backbones/build_backbone.py
@@ -35,7 +35,7 @@ def load_weights(model, model_name):
         sub_item = save_model_keys[0] if len(save_model_keys) == 1 else None
         state_dict = {k: v if v.size() == model_dict[k].size() else model_dict[k] for k, v in save_model[sub_item].items() if k in model_dict.keys()}
         if not state_dict or not sub_item:
-            print('Weights are not successully loaded. Check the state dict of weights file.')
+            print('Weights are not successfully loaded. Check the state dict of weights file.')
             return None
         else:
             print('Found correct weights in the "{}" item of loaded state_dict.'.format(sub_item))
