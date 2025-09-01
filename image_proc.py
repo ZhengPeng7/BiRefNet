e265606d6eedbca2a6b7c8ca950d372d45323e4e
何海明 <11165008@vivo.com>
2025-09-01 19:38:12 +0800

    修复图像增强模块里，添加高斯噪声的代码bug和常规顺序

--- image_proc.py
+++ image_proc.py
@@ -166,7 +166,8 @@ def random_gaussian(image, mean=0.1, sigma=0.35):
         return im
 
     img = np.asarray(image)
-    height, width, channels = img.shape
+    height, width = img.shape[:2]
+    channels = 1 if img.ndim == 2 else img.shape[2]
     img = gaussianNoisy(img[:].flatten(), mean, sigma)
     img = img.reshape([height, width, channels])
     return Image.fromarray(np.uint8(img))
