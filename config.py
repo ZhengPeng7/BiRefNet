3c1679c5da42a2dd4c0812d6855e8da51ef9e901
Peng Zheng <zhengpeng0108@gmail.com>
2025-09-26 15:40:00 +0800

    Update some default settings in config.py.

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ import math
 class Config():
     def __init__(self) -> None:
         # Main active settings
-        self.batch_size = 2                                     # Multi-GPU+BF16 training for 76GB / 62GB, without / with compile, on each A100.
-        self.compile = False                                     # 1. Trigger CPU memory leak in some extend, which is an inherent problem of PyTorch.
+        self.batch_size = 6                                     # Multi-GPU+BF16 training for 76GB / 62GB, without/with compile, on each A100.
+        self.compile = True                                     # 1. PyTorch<=2.0.1 has an inherent CPU memory leak problem; 2.0.1<PyTorch<2.5.0 cannot successfully compile.
         self.mixed_precision = ['no', 'fp16', 'bf16', 'fp8'][1] # 2. FP8 doesn't show acceleration in the torch.compile mode.
         self.SDPA_enabled = True                                # H200x1 + compile==True.  None: 43GB + 14s, math: 43GB + 15s, mem_eff: 35GB + 15s.
                                                                 # H200x1 + compile==False. None: 54GB + 25s, math: 51GB + 26s, mem_eff: 40GB + 25s.
